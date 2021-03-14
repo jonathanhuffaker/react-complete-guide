3d09418f3c999eae994abfd185812d148c7bb5fb
jonathanhuffaker <jonathanhuffaker@gmail.com>
2021-03-13 20:40:42 -0600

    Seeing the difference between using class vs a functional compponent.  Also have additional clickhandler

--- src/App.js
+++ src/App.js
@@ -14,12 +14,12 @@ const app = props => {
   const [otherState, setOtherState] = useState('some other value');
   console.log(personsState, otherState);
 
-  const switchNameHandler = () => {
+  const switchNameHandler = (newName) => {
     //console.log('Was Clicked!');
     //Dont do this:  this.state.persons[0].name = 'Maximilian';
     setPersonsState({
       persons: [
-      {name: 'Maximilian', age:'28'},
+      {name: newName, age:'28'},
       {name: 'Manu', age:'29'},
       {name: 'Stephanie', age: '27'}
     ]
@@ -29,9 +29,9 @@ const app = props => {
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
-        <button onClick={switchNameHandler}>Switch Name</button>
+        <button onClick={() => switchNameHandler.bind(this, 'Maximilian')}>Switch Name</button>
         <Person name={personsState.persons[0].name} age={personsState.persons[0].age} />
-        <Person name={personsState.persons[1].name} age={personsState.persons[1].age} >My Hobbies:  Racing</Person>
+        <Person name={personsState.persons[1].name} age={personsState.persons[1].age} click={switchNameHandler.bind(this, 'Max!')}>My Hobbies:  Racing</Person>
         <Person name={personsState.persons[2].name} age={personsState.persons[2].age} />
       </div>
     );
